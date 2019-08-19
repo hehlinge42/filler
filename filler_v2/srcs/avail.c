b1301c6359e3118edf6bab2aa85a308b6c23d6e1
Simon KPENOU <sikpenou@e1r10p14.42.fr>
2019-08-19 16:53:39 +0200

    avail.c, contains function to check if a piece can be placed at a given point

---
+++ filler_v2/srcs/avail.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   avail.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sikpenou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/19 16:44:50 by sikpenou          #+#    #+#             */
+/*   Updated: 2019/08/19 16:44:55 by sikpenou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int			is_available(t_var var, int o_x, int o_y)
+{
+	int		count;
+	int		x;
+	int		y;
+
+	count = 0;
+	y = 0;
+	while (count < 2 && y < var.y_piece)
+	{
+		x = 0;
+		while (count < 2 && x < var.x_piece)
+		{
+			if (var.piece[y][x] != '.' && (x + o_x >= var.x_map || y + o_y
+					>= var.y_map || var.map[o_y + y][o_x + x] == var.enemy
+						|| var.map[o_y + y][o_x + x] == var.enemy + 32))
+				count = 2;
+			else if (var.piece[y][x] == '*'
+				&& (var.map[o_y + y][o_x + x] == var.player
+					|| var.map[o_y + y][o_x + x] == var.player + 32))
+				count++;
+			x++;
+		}
+		y++;
+	}
+	return (count == 1 ? 1 : 0);
+}
+
