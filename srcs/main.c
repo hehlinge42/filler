978387a31a33c545f05d1817f2007bdb91b6ace9
Simon KPENOU <sikpenou@e1r10p14.42.fr>
2019-08-19 17:03:31 +0200

    added enemy definition to main

--- srcs/main.c
+++ srcs/main.c
@@ -6,86 +6,77 @@
 /*   By: hehlinge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 15:28:16 by hehlinge          #+#    #+#             */
-/*   Updated: 2019/07/11 19:08:51 by hehlinge         ###   ########.fr       */
+/*   Updated: 2019/08/19 14:34:53 by sikpenou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+#include "libft.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int		ft_parse_first_line(t_var *var)
 {
-	char	*str;
-	char	*tmp;
+	int		pos;
+	char	*line;
 
-	str = 0;
-	if (gnl(1, &str, 0) <= 0)
+	if (!(get_next_line(0, &line)))
 		return (0);
-	tmp = str;
-	if (ft_strncmp(tmp, "$$$ exec p", ft_strlen("$$$ exec p")))
+	pos = ft_strlen("$$$ exec p");
+	if (ft_strncmp(line, "$$$ exec p", pos))
 		return (0);
-	tmp += ft_strlen("$$$ exec p");
-	if (!((var->player = tmp[0]) == '1' || tmp[0] == '2'))
+	if (!(line[pos] != '1' || line[pos] != '2'))
 		return (0);
-	tmp++;
-	if (ft_strncmp(tmp, " : [players/", ft_strlen(" : [players/")))
+	var->player = line[pos] == '1' ? 'O' : 'X';
+	var->enemy = line[pos] == '1' ? 'X' : 'O';
+	pos++;
+	if (ft_strcmp(line + pos, " : [players/hehlinge.filler]"))
 		return (0);
-	tmp += ft_strlen(" : [players/");
-	if (ft_strcmp(tmp, "hehlinge.filler]"))
-		return (0);
-	if (str)
-		free(str);
+	ft_free((void **)&line);
 	return (1);
 }
 
-void	ft_display(int x, int y)
+int		ft_exit(int opt)
 {
-	ft_putnbr(x);
-	write(1, " ", 1);
-	ft_putnbr(y);
-	write(1, "\n", 1);
-}
-
-int		ft_exit(t_var *var, int opt)
-{
-	int		y;
-
-	y = -1;
-	while (++y < var->y_max)
-	{
-		if (var->map[y])
-			free(var->map[y]);
-		if (var->piece[y])
-			free(var->piece[y]);
-	}
 	if (opt == ERR_FIRST_LINE)
-		ft_putendl("Incorrect firstline");
+		printf("exit - bad first line\n");
 	else if (opt == BAD_MAP)
-		ft_putendl("Bad map");
+		printf("exit - bad map\n");
 	else if (opt == FULL_MAP)
-		ft_display(0, 0);
+		printf("0 0\n");
 	return (1);
+}
+
+int		ft_init_var(t_var *var)
+{
+	ft_memset(var, 0, sizeof(*var));
+	if ((var->points_x = (t_lst **)easymalloc(sizeof(*var->points_x)))
+		&& (var->points_o = (t_lst **)easymalloc(sizeof(*var->points_o)))
+		&& (var->points_n = (t_lst **)easymalloc(sizeof(*var->points_n))))
+		return (1);
+	return (0);
 }
 
 int		main(void)
 {
 	t_var	var;
 
-	ft_memset(&var, 0, sizeof(var));
-	if (!ft_parse_first_line(&var))
-		return (ft_exit(&var, ERR_FIRST_LINE));
-	ft_putendl("ft_parse_first_line ok");
-	while (1)
+	if (ft_init_var(&var))
 	{
-		if (!ft_parse_input(&var))
-			return (ft_exit(&var, BAD_MAP));
-		ft_print_debug(var);
-		return (1);
-		if (!place_piece())
-			return (ft_exit(&var, FULL_MAP));
-		ft_display(var.x, var.y);
+		if (!ft_parse_first_line(&var))
+			return (ft_exit(ERR_FIRST_LINE));
+		while (1)
+		{
+			if (!ft_parse_input(&var))
+				return (ft_exit(BAD_MAP));
+			ft_get_points(&var);
+			print_debug(var, "123");
+			return (1);
+//		display could be in place piece
+//		ft_place_piece();
+//		printf("%d %d\n", x, y);
+		}
 	}
 	return (0);
 }
